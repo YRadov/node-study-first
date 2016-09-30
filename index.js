cb5a186321b7de66a7f8c2a85240f7e024d48553
YRadov-ПК\YRadov <radov.yuriy@ukr.net>
2016-09-30 22:13:50 +0300

    add router

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
  * Created by YRadov on 30.09.2016.
  */
 var server = require("./server");
+var router = require("./router");
 
-server.start();
+server.start(router.route);
---
+++ router.js
@@ -0,0 +1,8 @@
+/**
+ * Created by YRadov on 30.09.2016.
+ */
+function route(pathname) {
+    console.log("About to route a request for " + pathname);
+}
+
+exports.route = route;
